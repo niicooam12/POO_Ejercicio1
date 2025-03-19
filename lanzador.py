294a6865b6fff9377baec95cf532c7c3398b5259
niicooam12 <nico.amador.montesinos@gmail.com>
2025-03-19 18:43:56 +0100

    Cambios

--- lanzador.py
+++ lanzador.py
@@ -3,3 +3,38 @@ from subclases.coche import Coche
 from subsubclases.motocicleta import Motocicleta
 from subsubclases.camioneta import Camioneta
 
+def lanzador_main():
+    vehiculos = []
+
+    vehiculos.append(Coche("rojo", 4, 150, 1200))
+    vehiculos.append(Coche("azul", 4, 200, 2000))
+    vehiculos.append(Bicicleta("verde", 2, "bh", "bmx", "urbana"))
+    vehiculos.append(Bicicleta("blanca", 2, "orbea", "r2", "deportiva"))
+    vehiculos.append(Camioneta("negro", 4, 100, 2200, "ford", "ranger", 1000))
+    vehiculos.append(Motocicleta("blanca",2,"Suzuki","GSXR", "Deportiva", 300, 999))
+    
+    
+    catalogar(vehiculos)
+    
+def catalogar(vehiculos):
+    for vehiculo in vehiculos:
+        print(f"Clase: {vehiculo.__class__.__name__}")
+        for atributo, valor in vehiculo.__dict__.items():
+            print(f"{atributo}: {valor}")
+        print()
+
+def catalogar(vehiculos, ruedas=None):
+    if ruedas is not None:
+        vehiculos_filtrados = [v for v in vehiculos if v.ruedas == ruedas]
+        print(f"Se han encontrado {len(vehiculos_filtrados)} vehículos con {ruedas} ruedas:")
+        for vehiculo in vehiculos_filtrados:
+            print(f"Clase: {vehiculo.__class__.__name__}")
+            for atributo, valor in vehiculo.__dict__.items():
+                print(f"{atributo}: {valor}")
+            print()
+    else:
+        for vehiculo in vehiculos:
+            print(f"Clase: {vehiculo.__class__.__name__}")
+            for atributo, valor in vehiculo.__dict__.items():
+                print(f"{atributo}: {valor}")
+            print()
